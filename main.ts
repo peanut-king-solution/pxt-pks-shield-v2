825f4ad9771ab28af5307c64942ffad3fa7ceb0f
AmberTang <72559487+TangYanYee@users.noreply.github.com>
2025-08-20 10:47:56 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1231,13 +1231,13 @@ namespace pksdriver {
     export function ultra_result(): number {
         let dist = 0;
         pins.i2cWriteNumber(0x57,0x01, NumberFormat.UInt8BE, false);
-        basic.pause(100);
-        let ul_raw = pins.i2cReadBuffer(0x57, 3, false);
-        dist = ul_raw[0] << 16 + ul_raw[1] << 8 + ul_raw[2];
+        basic.pause(120);
+        let ul_raw = pins.i2cReadBuffer(0x57,pins.sizeOf(NumberFormat.UInt8LE)* 3, false);
+        dist = ul_raw[0]*65536+ ul_raw[1]*256 + ul_raw[2];
         dist /= 1000;
         return dist;
     }
-
+   
     /**
     * Compass read function, to get the yaw angle
     */
