bc2b4828782294c9f2049d34b00fdce67eecf110
mkzank <celia.tyli@gmail.com>
2026-03-04 17:55:04 +0800

    updated test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
 pksdriver.motorStopAll()
 basic.forever(function () {
-    pksdriver.MotorRun(pksdriver.Motors.M1, pksdriver.Dir.CW, 80)
-    pksdriver.MotorRun(pksdriver.Motors.M2, pksdriver.Dir.CCW, 80)
+    pksdriver.motorRun(pksdriver.PKSDriverMotors.M1, pksdriver.PKSDriverDir.CW, 80)
+    pksdriver.motorRun(pksdriver.PKSDriverMotors.M2, pksdriver.PKSDriverDir.CCW, 80)
     basic.pause(1700)
   
     pksdriver.motorStopAll()
