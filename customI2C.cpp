ac8d10b6144cde4b3c5d26ca12fa68649f5a4760
jasoni <73955487+jasoni4399@users.noreply.github.com>
2026-06-02 18:16:03 +0800

    Update customI2C.cpp

--- customI2C.cpp
+++ customI2C.cpp
@@ -3,7 +3,11 @@
 namespace customI2C {
      //%
     void setI2CSpeedShim(int speed) {
+#if MICROBIT_CODAL
         uBit.i2c.setFrequency(speed);
+#else
+        uBit.i2c.frequency(speed);
+#endif
     }
 
 }
