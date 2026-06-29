6b1148ef3adeda120fdecfa512d8f3aff9531e03
jasoni <73955487+jasoni4399@users.noreply.github.com>
2026-06-29 16:54:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2674,8 +2674,8 @@ namespace pksdriver {
             _stepperMotorHBotMove(PKSHBotCardinalDirections.South)
         }
         //extra step to ensure fully reset, in case of any mechanical issues
-        stepperMotorHBotMove(PKSHBotCardinalDirections.West,150)
-        stepperMotorHBotMove(PKSHBotCardinalDirections.South, 150)
+        stepperMotorHBotMove(PKSHBotCardinalDirections.West,100, true)
+        stepperMotorHBotMove(PKSHBotCardinalDirections.South, 100, true)
     }
 
     /**
